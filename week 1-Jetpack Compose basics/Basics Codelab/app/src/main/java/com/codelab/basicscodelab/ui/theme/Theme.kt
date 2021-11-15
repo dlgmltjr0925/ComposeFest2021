e3d64e7045223bedfe5e7f232e10dda3344290bf
fine <dlgmltjr0925@gmail.com>
2021-11-15 16:46:19 +0900

    step4: 12 Styling and theming your app

--- week 1-Jetpack Compose basics/Basics Codelab/app/src/main/java/com/codelab/basicscodelab/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/Basics Codelab/app/src/main/java/com/codelab/basicscodelab/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,11 +14,15 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    surface =  Blue,
+    onSurface = Color.White,
+    primary = Navy,
+    onPrimary = Chartreuse
 
-    /* Other default colors to override
+
+/* Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
