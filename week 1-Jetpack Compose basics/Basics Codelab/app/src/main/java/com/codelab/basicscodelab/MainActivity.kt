d2bc4e6da53cc4ed661b17d8bdaad3bd76846572
fine <dlgmltjr0925@gmail.com>
2021-11-15 15:12:02 +0900

    step4: 1 ~ 4 Tweaking the UI

---
+++ week 1-Jetpack Compose basics/Basics Codelab/app/src/main/java/com/codelab/basicscodelab/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.codelab.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelab.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting(name = "Android")
+        }
+    }
+}
