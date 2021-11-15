72aec23f591516b84a78071535e5fa846c7dc73e
fine <dlgmltjr0925@gmail.com>
2021-11-15 13:14:36 +0900

    강의 1: 구성 가능한 함수

---
+++ week 1-Jetpack Compose basics/Tutorial Jetpack Compose basic/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.codelab.basics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.basics.ui.theme.TutorialJetpackComposeBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text("Hello $name")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android!")
+}
