638c31c8df007be75ddc123ce7837a5ea41c8a41
fine <dlgmltjr0925@gmail.com>
2021-11-16 11:17:31 +0900

    step6: 6 Working with lists

--- week 2-1-Layouts in JetpackCompose/layouts/app/src/main/java/com/codelab/layouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/layouts/app/src/main/java/com/codelab/layouts/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.codelab.layouts
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -13,22 +15,110 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.codelab.layouts.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                LayoutsCodelab()
+                LazyList()
             }
         }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ), contentDescription = "Android Logo", modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #${index}", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun LazyList() {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(index = it)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyListPreview() {
+    LayoutsCodelabTheme {
+        LazyList()
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text(text = "Item #${it + 1}")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleListPreview() {
+    LayoutsCodelabTheme {
+        SimpleList()
     }
 }
 
@@ -50,7 +140,11 @@ fun LayoutsCodelab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
